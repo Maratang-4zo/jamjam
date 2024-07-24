a72104273d6da1167b9819a74e3550af6ff50b26
minju412 <ln8847@ajou.ac.kr>
2024-07-24 17:53:15 +0900

    feat: 전국 지하철역에 대해 상권정보 크롤링
    - csv 파일에서 지하철역 이름을 읽어옴
    - 읽어온 데이터를 파싱하여 db에 저장
    - 시간을 단축하기 위해 리팩토링 예정

--- crawling/map_info/apis/api.py
+++ crawling/map_info/apis/api.py
@@ -1,19 +1,32 @@
-from map_info.kakao_map_crawling import getPlaces
+from datetime import datetime
+
+from map_info.kakao_map_crawling import get_places
 from map_info import app, db
-from flask import jsonify
+from flask import jsonify, request
 from ..models.place import Place
 
 # 지도 데이터 크롤링 후 db 저장
 @app.route('/crawling', methods=["POST"])
 def addData():
 
-    data = getPlaces('스터디룸', 37.4996, 127.0369) # 중심 위도, 경도 (역삼역)
+    # keywords = ['스터디룸', '식당', '카페']
+    keywords = ['공원']
+    for keyword in keywords:
+        data = get_places(keyword)
+        if isinstance(data, list): # data가 리스트 형태로 반환되었는지 확인
 
-    if isinstance(data, list): # data가 리스트 형태로 반환되었는지 확인
-        for item in data:
-            new_result = Place(**item)
-            db.session.add(new_result)
-        db.session.commit()
-        return jsonify({"response": "complete"}), 201
-    else:
-        return jsonify({"error": "Invalid data format"}), 400
+            # TODO: 여기서 select all 로 한번에 받아오기
+
+            for item in data:
+                # db에는 있는 정보인데 api에서 받아오지 못하면 -> 폐업한 곳 -> is_delete를 True로 업데이트
+                existing_place = Place.query.filter_by(id=item['id']).first()
+                if existing_place:
+                    existing_place.is_delete = True
+                    existing_place.update_time = datetime.now()
+                else:
+                    new_result = Place(**item)
+                    db.session.add(new_result)
+            db.session.commit()
+        else:
+            return jsonify({"error": "Invalid data format"}), 400
+    return jsonify({"response": "complete"}), 201
--- crawling/map_info/kakao_map_crawling.py
+++ crawling/map_info/kakao_map_crawling.py
@@ -1,40 +1,67 @@
+import os
+import pandas as pd
 import requests
 from config import KAKAO_REST_API_KEY, KAKAO_SEARCH_KEYWORD_URL
 
-def getPlaces(keyword, center_lat, center_lon):
+# Load the CSV file
+file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'subwayinfo.csv')
+df = pd.read_csv(file_path, header=None, names=['C1', 'C2', 'C3', 'C4', 'C5'])
+
+
+
+# 지하철 이름으로 위/경도 반환
+def get_station_info(station_name):
+    try:
+        # 첫번째로 만나는 역 정보 반환
+        station_row = df[df['C1'] == station_name].iloc[0]
+        return station_row['C3'], station_row['C4']
+    except IndexError:
+        return None, None
+
+def get_places(keyword):
 
     # 요청 헤더
     headers = {
         'Authorization': f'KakaoAK {KAKAO_REST_API_KEY}'
     }
 
-    # 요청 파라미터
-    params = {
-        'query': keyword,
-        'x': center_lon,
-        'y': center_lat,
-        'radius': 1000,  # 반경 1km (미터 단위)
-        'size': 5  # 한 페이지에 가져올 결과 수 (최대 15개)
-        # 'sort': 'distance'  # 정렬 옵션 (거리순)
-    }
-
-    response = requests.get(KAKAO_SEARCH_KEYWORD_URL, headers=headers, params=params) # API 호출
-
-    data = response.json() # JSON 응답
-
-    # 파싱
     places = []
-    for place in data['documents']:
-        print(place)
-        print('---------------------------------')
-        place_info = {
-            'name': place['place_name'],
-            'road_address': place['road_address_name'],
-            'address': place['address_name'],
-            'phone': place.get('phone', '정보 없음'),
-            'latitude': place['y'],
-            'longitude': place['x']
+
+    for station_name in df['C1']:
+
+        # 요청 파라미터
+        center_lon, center_lat = get_station_info(station_name)
+
+        if center_lon is None or center_lat is None:
+            print(f"Coordinates for {keyword} not found.")
+            return []
+
+        params = {
+            'query': keyword,
+            'x': center_lat,
+            'y': center_lon,
+            'radius': 1000,  # 반경 1km (미터 단위)
+            'size': 1  # 한 페이지에 가져올 결과 수 (최대 15개)
+            # 'sort': 'distance'  # 정렬 옵션 (거리순)
         }
-        places.append(place_info)
+
+        response = requests.get(KAKAO_SEARCH_KEYWORD_URL, headers=headers, params=params) # API 호출
+
+        data = response.json() # JSON 응답
+
+        # 파싱
+        for place in data['documents']:
+            place_info = {
+                'station_name': station_name,
+                'name': place['place_name'],
+                'id': place['id'],
+                'category': keyword,
+                'road_address': place['road_address_name'],
+                'address': place['address_name'],
+                'phone': place.get('phone', '정보 없음'),
+                'latitude': place['y'],
+                'longitude': place['x']
+            }
+            places.append(place_info)
 
     return places

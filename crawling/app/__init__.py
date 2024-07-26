6e46f962a3827288b3e9a0f667c9f488ddc918df
minju412 <ln8847@ajou.ac.kr>
2024-07-26 11:14:34 +0900

    feat: 데이터베이스 테이블 생성 코드 추가
    - 기존에는 sqlalchemy를 사용하여 모델을 통해 자동으로 테이블을
      생성했습니다
    - sqlalchemy를 사용하지 않도록 바꾸면서 sql로 테이블을 직접 생성해야
      합니다

--- crawling/app/__init__.py
+++ crawling/app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
-# from app.config import Config
 app = Flask(__name__)
-# app.config.from_object(Config)
 
 # 데이터베이스 연결 초기화
 from app.db.connection import db
--- crawling/app/db/connection.py
+++ crawling/app/db/connection.py
@@ -1,15 +1,41 @@
 import pymysql
 from app.config import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 
+
 def get_db_connection():
     connection = pymysql.connect(
-        host=f'{DB_HOST}',     # MySQL Server Address
-        port=DB_PORT,     # MySQL Server Port
+        host=f'{DB_HOST}',          # MySQL Server Address
+        port=DB_PORT,               # MySQL Server Port
         user=f'{DB_USERNAME}',      # MySQL username
         passwd=f'{DB_PASSWORD}',    # password for MySQL username
-        db=f'{DB_NAME}',   # Database name
+        db=f'{DB_NAME}',            # Database name
         charset='utf8'
     )
     return connection
 
+
+def create_table():
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS place_tb (
+        place_id INT AUTO_INCREMENT PRIMARY KEY,
+        station_name VARCHAR(255),
+        name VARCHAR(255),
+        id INT,
+        category VARCHAR(255),
+        road_address VARCHAR(255),
+        address VARCHAR(255),
+        phone VARCHAR(20),
+        latitude FLOAT,
+        longitude FLOAT,
+        is_delete TINYINT(1),
+        create_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        update_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+    """
+    with db.cursor() as cursor:
+        cursor.execute(create_table_query)
+        db.commit()
+
+
 db = get_db_connection()
+# create_table()

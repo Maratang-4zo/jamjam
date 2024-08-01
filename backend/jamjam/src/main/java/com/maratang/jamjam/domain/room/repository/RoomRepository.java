c60c85e134afbf8d8b19beaeb781d1e0a0b7e00e
han <chos9715@naver.com>
2024-08-01 15:41:49 +0900

    ✨ Feat: 음성채팅 참여 기능
    - 파라미터 변경
    - 방 유효성 검사 추가
    #S11P12A604-66

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/room/repository/RoomRepository.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/room/repository/RoomRepository.java
@@ -8,6 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.maratang.jamjam.domain.room.entity.Room;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-
 	Optional<Room> findByRoomUUID(UUID roomUUID);
 }

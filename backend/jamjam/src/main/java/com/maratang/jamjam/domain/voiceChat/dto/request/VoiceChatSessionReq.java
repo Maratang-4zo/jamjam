3214aa099a6f8d4c8243958c9e4331fdec902f46
han <chos9715@naver.com>
2024-08-01 15:41:49 +0900

    ✨ Feat: 음성채팅 참여 기능
    - 파라미터 변경
    - 방 유효성 검사 추가
    #S11P12A604-66

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/voiceChat/dto/request/VoiceChatSessionReq.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/voiceChat/dto/request/VoiceChatSessionReq.java
@@ -1,7 +1,11 @@
 package com.maratang.jamjam.domain.voiceChat.dto.request;
 
+import java.util.UUID;
+
 import lombok.Getter;
 
 @Getter
 public class VoiceChatSessionReq {
+	UUID roomUUID;
+	UUID attendeeUUID;
 }
--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/voiceChat/dto/request/VoiceChatTokenReq.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/voiceChat/dto/request/VoiceChatTokenReq.java
@@ -1,7 +1,11 @@
 package com.maratang.jamjam.domain.voiceChat.dto.request;
 
+import java.util.UUID;
+
 import lombok.Getter;
 
 @Getter
 public class VoiceChatTokenReq {
+	UUID roomUUID;
+	UUID attendeeUUID;
 }

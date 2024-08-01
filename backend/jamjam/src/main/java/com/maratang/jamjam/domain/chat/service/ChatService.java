9903157da77163a3e6d736de83ede3e7bbf659d6
han <chos9715@naver.com>
2024-08-01 16:34:55 +0900

    ✨ Feat: 채팅 보내기 구현
    - 파라미터 변경
    #S11P12A604-306

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/chat/service/ChatService.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/chat/service/ChatService.java
@@ -1,7 +1,5 @@
 package com.maratang.jamjam.domain.chat.service;
 
-import java.util.UUID;
-
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,23 +12,26 @@ import com.maratang.jamjam.domain.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor_ = {@Lazy})
 public class ChatService {
 
 	private final RoomRepository roomRepository;
 	private final ChatRepository chatRepository;
-	private final SimpMessagingTemplate simpMessagingTemplate;
+	private final AttendeeRepository attendeeRepository;
+	@Lazy private final SimpMessagingTemplate simpMessagingTemplate;
+	private final String ROOM_SUBSCRIBE_DEST = "/sub/rooms/";
 
 	private static final ChatMapper mapper = ChatMapper.INSTANCE;
 
-	public void sendMessage(UUID roomId, ChatReq chatReq) {
-		// Room room = roomRepository.findById(roomId).orElseThrow(() -> new BusinessException(ErrorCode.ROOM_NOT_FOUND));
+	public void sendMessage(UUID roomUUID, UUID attendeeUUID, ChatReq chatReq) {
+		Room room = roomRepository.findByRoomUUID(roomUUID).orElseThrow(() -> new BusinessException(ErrorCode.ROOM_NOT_FOUND));
+		Attendee attendee = attendeeRepository.findByAttendeeUUID(attendeeUUID).orElseThrow(() -> new BusinessException(ErrorCode.ATTENDEE_NOT_FOUND));
 		// member
-		// Chat chat = mapper.chatReqToChat(chatReq, room);
-		// chatRepository.save(chat);
+		Chat chat = mapper.chatReqToChat(chatReq, room, attendee);
+		chatRepository.save(chat);
 
-		ChatRes  res = ChatRes.builder().nickname(chatReq.getNickname()).content(chatReq.getContent()).type("CHAT_RECEIVED").build();
+		ChatRes res = ChatRes.builder().attendeeUUID(attendeeUUID).content(chatReq.getContent()).type("CHAT_RECEIVED").build();
 		// chat to chatReq
-		simpMessagingTemplate.convertAndSend("/sub/rooms/" + roomId, res);
+		simpMessagingTemplate.convertAndSend(ROOM_SUBSCRIBE_DEST + roomUUID, res);
 	}
 }

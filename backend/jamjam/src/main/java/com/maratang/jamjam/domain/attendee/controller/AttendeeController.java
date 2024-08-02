375f65959fc078024af65c9d5d592f29f0962379
ckm7907 <ckm7907@naver.com>
2024-08-02 09:28:42 +0900

    🔥 Hotfix: roomToken-cookie httpOnly 설정 제거
    #

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/attendee/controller/AttendeeController.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/attendee/controller/AttendeeController.java
@@ -35,7 +35,6 @@ public class AttendeeController {
 		RoomJwtTokenDto roomJwtTokenDto = roomTokenProvider.createRoomJwtToken(roomJwtTokenCliams);
 
 		Cookie cookie = new Cookie("roomToken", roomJwtTokenDto.getRoomToken());
-		cookie.setHttpOnly(true);
 		cookie.setPath("/");
 
 		response.addCookie(cookie);

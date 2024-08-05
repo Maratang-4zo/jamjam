b6edc765ff8c109fdad3773c2e8416c1a8a8730a
ckm7907 <ckm7907@naver.com>
2024-08-05 13:00:14 +0900

    ✨ feat: room get 형식 변경 완료
    - room get 형식 변경

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/room/dto/response/RoomGetRes.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/room/dto/response/RoomGetRes.java
@@ -1,10 +1,23 @@
 package com.maratang.jamjam.domain.room.dto.response;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import com.maratang.jamjam.domain.attendee.dto.AttendeeDTO;
+import com.maratang.jamjam.global.station.SubwayInfo;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class RoomGetRes {
-    private boolean isRoom;
+    UUID RoomUUID;
+    String roomName;
+    SubwayInfo roomCenterStart;
+    LocalDateTime roomTime;
+    String roomPurpose;
+    UUID hostUUID;
+    List<AttendeeDTO> attendees;
 }

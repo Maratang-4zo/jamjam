3ea43d59151721406747cba05dc0aada1bec54ae
minju412 <ln8847@ajou.ac.kr>
2024-08-07 10:34:05 +0900

    feat: 캐싱을 적용하여 조회 성능 개선
    - 지하철 역 주변 상권 정보를 가져올 때 캐싱 적용
    - [지하철역이름-카테고리]를 키 값으로 응답 데이터를 redis에 저장
    - ngrinder를 이용하여 성능 측정 및 시각화

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/localInfo/dto/response/LocalInfoRes.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/localInfo/dto/response/LocalInfoRes.java
@@ -2,11 +2,20 @@ package com.maratang.jamjam.domain.localInfo.dto.response;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocalInfoRes {
 	private String name;
 	private String category;
@@ -15,6 +24,7 @@ public class LocalInfoRes {
 	private String phone;
 	private Float latitude;
 	private Float longitude;
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	private LocalDateTime updatedAt;
-
 }

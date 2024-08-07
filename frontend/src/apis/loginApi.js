8098e8f9dd4a468a64a21a475f0393d9dab359a9
ckm7907 <ckm7907@naver.com>
2024-08-07 16:27:05 +0900

    ✨ feat: FE 로그인 axios 구현
    - 로그인 axios 구현 완료
    - #S11P12A604-398

--- frontend/src/apis/loginApi.js
+++ frontend/src/apis/loginApi.js
@@ -1,20 +1,50 @@
-import axios from "axios";
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
+// 쿠키에서 accessToken을 가져오는 함수
+const getAccessTokenFromCookie = () => {
+  return Cookies.get('accessToken');
+};
+
+// Axios 인스턴스 생성
+const axiosInstance = axios.create({
+  baseURL: `http://localhost:8080`,
+  withCredentials: true,
+});
+
+// 요청 인터셉터 설정
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = getAccessTokenFromCookie(); // 쿠키에서 토큰을 가져옴
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`; // Authorization 헤더에 토큰 추가
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 사용자 정보 가져오기 함수
 export const getUserInfo = async () => {
   try {
-    const response = await axios.get("/api/members/info");
+    const response = await axiosInstance.get('/api/members/info');
     return response.data;
   } catch (error) {
-    console.error("사용자 정보 가져오기 실패", error);
+    console.error('사용자 정보 가져오기 실패', error);
     throw error;
   }
 };
 
+// 닉네임 업데이트 함수
 export const updateUserNickname = async (nickname) => {
   try {
-    await axios.post("/api/members/info", { nickname });
+    await axiosInstance.post('/api/members/info', { nickname });
   } catch (error) {
-    console.error("닉네임 수정 실패", error);
+    console.error('닉네임 수정 실패', error);
     throw error;
   }
 };

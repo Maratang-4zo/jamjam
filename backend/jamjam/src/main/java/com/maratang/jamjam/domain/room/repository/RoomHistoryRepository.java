02486a344926c3e8bd20179141d2028d8d79fe6c
hwang <hyb26@naver.com>
2024-08-07 17:13:10 +0900

    fix: RoomHistoryRepository, mapper 삭제
    - 필요없는 파일 삭제
    - #S11P12A604-72

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/room/repository/RoomHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.maratang.jamjam.domain.room.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.maratang.jamjam.domain.room.entity.Room;
-
-public interface RoomHistoryRepository extends JpaRepository<Room, Long> {
-}

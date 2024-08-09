8192ac403a7bbe78df2098e0400ff944c5299020
ckm7907 <ckm7907@naver.com>
2024-08-09 15:07:59 +0900

    :hammer: Fix: GameRecord Id -> UUID 변경 요
    - #S11P12A604-447

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/roundRecord/dto/request/RoundRecordCreateReq.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/roundRecord/dto/request/RoundRecordCreateReq.java
@@ -1,5 +1,7 @@
 package com.maratang.jamjam.domain.roundRecord.dto.request;
 
+import java.util.UUID;
+
 import lombok.Getter;
 
 @Getter
@@ -7,6 +9,6 @@ public class RoundRecordCreateReq {
 
     private Integer round;
     private Long gameId;
-    private Long gameRecordId;
+    private UUID gameRecordUUID;
     private String stationName;
 }
--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/roundRecord/dto/request/RoundRecordGetReq.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/roundRecord/dto/request/RoundRecordGetReq.java
@@ -1,9 +1,11 @@
 package com.maratang.jamjam.domain.roundRecord.dto.request;
 
+import java.util.UUID;
+
 import lombok.Getter;
 
 @Getter
 public class RoundRecordGetReq {
 
-    private Long gameRecordId;
+    private UUID gameRecordUUID;
 }

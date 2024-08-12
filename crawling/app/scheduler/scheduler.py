8b75bde56963d9a116a4523c9df389ce794ff394
minju412 <ln8847@ajou.ac.kr>
2024-08-12 15:55:11 +0900

    feat: schema 추가
    - 테이블 생성을 위해 schema 추가
    - keyword 추가 (기존 3개 -> 10개)
    - Dockerfile에 timezone 설정 추가
    - 스케줄링 코드에 로그 추가

--- crawling/app/scheduler/scheduler.py
+++ crawling/app/scheduler/scheduler.py
@@ -4,22 +4,28 @@ from app.crawler.crawler import crawling_places
 from app.models.model import LocalInfo
 from apscheduler.schedulers.background import BackgroundScheduler
 from app import app
+import logging
 
 
 # 스케줄러 작업 함수
 def scheduler_add_data():
-    print("Scheduler_Add_Data is alive!")
+
+    logging.info("Scheduler_Add_Data is alive!")
+
     start_time = time.time()  # 시작 시간 측정
 
     with app.app_context():  # 애플리케이션 컨텍스트 설정
         all_data = []
+
+        # DB에 있는 Place 데이터 가져오기
+        existing_data = LocalInfo.get_all_places()
 
         data = crawling_places() # API를 통해 데이터 수집
 
         if isinstance(data, list):  # data가 리스트 형태로 반환되었는지 확인
             all_data.extend(data)
         else:
-            print("Invalid data format")
+            logging.info("Invalid data format")
             return
 
         end_kakao_request = time.time()  # 카카오맵 API로 장소 요청 후 받아오는 시간 측정
@@ -27,9 +33,6 @@ def scheduler_add_data():
         # API로 가져온 새로운 데이터를 DataFrame으로 변환
         new_data_df = pd.DataFrame(all_data)
         new_data_df['id'] = new_data_df['id'].astype(str)
-
-        # DB에 있는 Place 데이터 가져오기
-        existing_data = LocalInfo.get_all_places()
 
         if len(existing_data) == 0:  # DB에 아무 데이터가 없다면 새로운 데이터 삽입
             LocalInfo.bulk_insert(new_data_df.to_dict(orient='records'))
@@ -40,9 +43,9 @@ def scheduler_add_data():
 
         end_db_update = time.time()
 
-        print(f"총 경과 시간: {end_db_update - start_time} seconds")
-        print(f"카카오맵 api에 장소 리퀘스트 소요 시간: {end_kakao_request - start_time} seconds")
-        print(f"db status 업데이트 소요 시간: {end_db_update - end_kakao_request} seconds")
+        logging.info(f"총 경과 시간: {end_db_update - start_time} seconds")
+        logging.info(f"카카오맵 api에 장소 리퀘스트 소요 시간: {end_kakao_request - start_time} seconds")
+        logging.info(f"db status 업데이트 소요 시간: {end_db_update - end_kakao_request} seconds")
 
 
 def update_database(new_data_df, existing_data_df):
@@ -70,6 +73,12 @@ def update_database(new_data_df, existing_data_df):
 
 # 스케줄러 시작 함수
 def start_scheduler():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
     # timezone을 설정해두지 않으면 경고 문구가 뜰 수 있다.
     # BackgroundScheduler을 통해 schedule 인스턴스를 생성한다.
     schedule = BackgroundScheduler(daemon=True, timezone='Asia/Seoul')
@@ -79,5 +88,6 @@ def start_scheduler():
     schedule.add_job(scheduler_add_data, 'cron', day_of_week='mon', hour=3, minute=0)
 
     # 스케줄을 start()로 호출한다
+    logging.info("Scheduler Start Soon!")
     schedule.start()
 

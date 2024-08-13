02a4887d84ef9dd823379b93f57886638d07fd45
thunthuni1004 <thunthuni1004@gmail.com>
2024-08-13 19:55:07 +0900

    400error in join room

--- frontend/src/routes/JoinRoom.jsx
+++ frontend/src/routes/JoinRoom.jsx
@@ -299,6 +299,7 @@ function JoinRoom() {
 
       navigate(`/room/${roomUUID}`);
     } catch (error) {
+      console.log(data);
       console.log("방 참여 실패", error);
     }
   };
@@ -345,7 +346,7 @@ function JoinRoom() {
                 {nicknameValue.length}/{maxChars}
               </CharCount>
             </NicknameInput>
-            <EnterButton>
+            <EnterButton type="submit">
               <p>접속</p>
             </EnterButton>
           </Form>

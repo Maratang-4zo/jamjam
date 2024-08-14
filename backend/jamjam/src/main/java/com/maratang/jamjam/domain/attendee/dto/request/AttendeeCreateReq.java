ca50b8c8150a82f653b55bde35067c6d1f188af6
ckm7907 <ckm7907@naver.com>
2024-08-14 19:50:34 +0900

    Hotfix: AttendeeCreateReq 잘못된 부분 수정

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/attendee/dto/request/AttendeeCreateReq.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/attendee/dto/request/AttendeeCreateReq.java
@@ -2,11 +2,11 @@ package com.maratang.jamjam.domain.attendee.dto.request;
 
 import com.maratang.jamjam.domain.attendee.entity.Attendee;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class AttendeeCreateReq {
 	private String nickname;
 

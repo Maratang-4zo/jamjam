82d789c214f91b28e1d0816f65e2dc242093f78f
han <chos9715@naver.com>
2024-08-14 21:57:50 +0900

    🔥 Hotfix: constructor
    - 로컬은 되는데요...
    #

--- backend/jamjam/src/main/java/com/maratang/jamjam/domain/attendee/dto/request/AttendeeCreateReq.java
+++ backend/jamjam/src/main/java/com/maratang/jamjam/domain/attendee/dto/request/AttendeeCreateReq.java
@@ -1,16 +1,21 @@
 package com.maratang.jamjam.domain.attendee.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.maratang.jamjam.domain.attendee.entity.Attendee;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class AttendeeCreateReq {
 	private String nickname;
+
+	@JsonCreator
+	public AttendeeCreateReq(@JsonProperty("nickname") String nickname) {
+		this.nickname = nickname;
+	}
 
 	public Attendee toEntity() {
 		return Attendee.builder()
